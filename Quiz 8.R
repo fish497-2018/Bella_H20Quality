12466dd1e1cfbd2372e0c489c39d93b1c270d642
Isabella Colpo <isabellacolpo@D-10-19-116-33.dhcp4.washington.edu>
2018-05-18 13:43:49 -0700

    Did the same process to select and filter the pertinent columns for the water quality attributes for the remaining location.  Then, combined the three location datasets into one "full_data" using bind_rows function.

--- Quiz 8.R
+++ Quiz 8.R
@@ -16,3 +16,21 @@ july_psbasin <- select(july_psbasin, depth..meters., salinity..psu., temperature
 #
 #Print modified data table with depth, salinity, temperature, chlorophyll, dissolved oxygen, and pH
 print(july_psbasin)
+#
+#Rename and read Port Townsend data to reflect the same columns as the july_psbasin data
+pt <- read.csv("PortTownsend_16")
+pt <- PortTownsend_16
+View(pt)
+july_pt <- filter(pt, date == "7/6/16")
+july_pt <- select(july_pt, depth..meters., salinity..psu., temperature..centigrade., chlorophyll.raw..ug.l., dissolved.oxygen..mg.l..raw, pH)
+View (july_pt)
+#Repeat same process as I dud with the Port Townsend and Puget Sound Basin datasets with the third dataset: Elliot Bay
+View(ElliotBay_16)
+eb <- read.csv("ElliotBay_16")
+eb <- ElliotBay_16
+View(eb)
+july_eb <- filter(eb, date == "7/11/16")
+july_eb <- select(july_eb, depth..meters., salinity..psu., temperature..centigrade., chlorophyll.raw..ug.l., dissolved.oxygen..mg.l..raw, pH)
+View (july_eb)
+#Combined the three datasets into one large dataset using the bind_rows function from dplyr, and renamed the three datasets to specify the locations and used .id to create a new location column.
+full_data <- bind_rows(list(psbasin = july_psbasin, eb = july_eb, pt = july_pt), .id = "location")
